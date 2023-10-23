53fbbdc0c3ccf457705afc794d531abfc7e5ae30
Isthifaa <139338592+Isthifaa@users.noreply.github.com>
2023-10-23 18:24:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Function to display the inputted data in a popup
-function showPopup() {
+
+function openPopup() {
     var firstName = document.getElementById("fname").value;
     var lastName = document.getElementById("lname").value;
     var dateOfBirth = document.getElementById("dateofbirth").value;
@@ -10,25 +10,46 @@ function showPopup() {
     var phoneNumber = document.getElementById("number").value;
 
     var popupContent = `
-    First Name: ${firstName}
-    Last Name: ${lastName}
-    Date of Birth: ${dateOfBirth}
-    Country: ${country}
-    Gender: ${gender}
-    Profession: ${profession}
-    Email: ${email}
-    Phone Number: ${phoneNumber}
-`;
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Date of Birth:</strong> ${dateOfBirth}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
+        <p><strong>Profession:</strong> ${profession}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phoneNumber}</p>
+    `;
 
-alert(popupContent);
+    document.getElementById("popup-content").innerHTML = popupContent;
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the pop-up
+function closePopup() {
+   
+    document.getElementById("fname").value = '';
+    document.getElementById("lname").value = '';
+    document.getElementById("dateofbirth").value = '';
+    document.getElementById("country").value = '';
+    document.querySelector('input[name="male"]').checked = false;
+    document.querySelector('input[name="female"]').checked = false;
+    document.getElementById("profession").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("number").value = '';
+
+   
+    document.getElementById("popup").style.display = "none";
+}
 
 document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
-    showAlert(); // Display the alert pop-up
+    event.preventDefault();
+    openPopup(); 
 });
-}
 
-// Attach the resetForm function to the reset button
-function rest() {
-    document.getElementById("form").reset();
-}
+
+
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    openPopup();
+});
+
